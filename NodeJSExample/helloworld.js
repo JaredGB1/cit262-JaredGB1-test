495fde7c06e22caf783661dac8de1fbe4d41a4b9
JaredGB1 <mjaredgb@gmail.com>
2024-05-25 23:02:14 -0600

    Sprint 2 Activity
    Created branch first_test, install Jest and created test for the hello world program

--- NodeJSExample/helloworld.js
+++
@@ -1,2 +0,0 @@
-const firstName="Mynor";
-console.log("Hello ___");
---
+++ NodeJSExample/src/helloworld.js
@@ -0,0 +1,9 @@
+const firstName = "Mynor";
+
+function helloWorld() {
+  return `Hello ___`;
+}
+
+console.log(helloWorld());
+
+module.exports = helloWorld;
